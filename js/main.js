108da82e122e29003d40712845ffdf3899595bf9
NamanAg30 <47788349+NamanAg30@users.noreply.github.com>
2020-08-23 23:00:27 +0530

    http to https

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ function movieSelected(id){
 function getMovie(){
   let movieId = sessionStorage.getItem('movieId');
 
-  axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=thewdb')
+  axios.get('https://www.omdbapi.com?i='+movieId+'&apikey=thewdb')
     .then((response) => {
       console.log(response);
       let movie = response.data;
